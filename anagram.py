9f9e6b8b2b0ed9d94e63a93288e397e56961629c
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-12-03 12:57:46 -0600

    anagram done

--- anagram.py
+++ anagram.py
@@ -4,21 +4,24 @@ def makeAnagram(array_1, array_2):
     a = array_1
     b = array_2
     i = 0
+    q = str()
 
     for letter_a in a:
         if letter_a not in b:
             i += 1
-        if letter_a in b:
+        if letter_a in b and letter_a not in q:
             a_count = a.count(letter_a)
             b_count = b.count(letter_a)
+            i += a_count - b_count
+            q += letter_a
 
     for letter_b in b:
         if letter_b not in a:
             i += 1
 
-    print(a, b, i)
+    print(i)
 
-this = 'bigbluebabies'
-that = 'bluebitches'
+this = 'bibgig'
+that = 'blg'
 
 makeAnagram(this, that)
